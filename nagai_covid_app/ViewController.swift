cd08e7ae0c869999b5c649690bd7d889b8c396a6
ryouji nagai <>
2020-09-12 15:33:56 +0900

    first commit

---
+++ nagai_covid_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nagai_covid_app
+//
+//  Created by ryouji nagai on 2020/09/12.
+//  Copyright © 2020 ryouji nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nagai_covid_appTests/nagai_covid_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  nagai_covid_appTests.swift
+//  nagai_covid_appTests
+//
+//  Created by ryouji nagai on 2020/09/12.
+//  Copyright © 2020 ryouji nagai. All rights reserved.
+//
+
+import XCTest
+@testable import nagai_covid_app
+
+class nagai_covid_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
