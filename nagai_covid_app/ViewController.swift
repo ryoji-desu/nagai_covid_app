4b8dc3da8a648db58f12c404f2cf0a6bb88bff85
ryouji nagai <>
2020-09-14 11:00:57 +0900

    ラベルとボタンを出力

--- nagai_covid_app/ViewController.swift
+++ nagai_covid_app/ViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("swift wakaritai")
+        let titleLabel = UILabel()
+        titleLabel.frame = CGRect(x: 0, y: 20, width: UIScreen.main.bounds.size.width, height: 44) // 位置とサイズの指定
+        titleLabel.textAlignment = NSTextAlignment.center // 横揃えの設定
+        titleLabel.text = "アプリのタイトル" // テキストの設定
+        titleLabel.textColor = UIColor.black // テキストカラーの設定
+        titleLabel.font = UIFont(name: "HiraKakuProN-W6", size: 17) // フォントの設定
+        self.view.addSubview(titleLabel)
+        
+        let button:UIButton = UIButton(frame: CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.height / 4))
+        button.backgroundColor = .black
+        button.setTitle("button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        self.view.addSubview(button)
     }
-
-
 }
 
